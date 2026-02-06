fd5c2966cdc293f514faafb028e66516472fc6de
alecamargo33-code <alecamargo33@gmail.com>
2026-02-06 16:45:37 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+df = pd.read_excel("https://raw.githubusercontent.com/alecamargo33-code/esocialnovo/refs/heads/master/eventos_esocial.xlsx")
+
 st.set_page_config(
     page_title="Indicadores eSocial",
     layout="wide"
@@ -9,10 +11,18 @@ st.set_page_config(
 st.title("Gestão eSocial")
 st.write("Dashboard local")
 
-df = pd.DataFrame(
-    {"Categoria":["A","B","C","D"],
-     "Valor":[10,25,40,55]
-     }
-)
+st.sidebar.header("Filtros")
 
-st.bar_chart(df.set_index("Categoria"))
+#Filtro de ano
+anos_disponiveis = sorted(df['Ano'].unique())
+anos_selecionados = st.sidebar.multiselect("Ano", anos_disponiveis, default=anos_disponiveis)
+
+#Filtro de evento
+eventos_disponiveis = sorted(df['Código Evento'].unique())
+eventos_selecionados = st.sidebar.multiselect("Evento", eventos_disponiveis, default=eventos_disponiveis)
+
+#Filtro de status do evento
+status_disponiveis = sorted(df['Status Evento'].astype(str).unique())
+status_selecionados = st.sidebar.multiselect("Status do Evento", status_disponiveis, default=status_disponiveis)
+
+
